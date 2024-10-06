7f0f4c854d5da95291a4bfb1b0d3105d661898af
Dhanushka Nuwan Premasiri <97797596+DhanushkaNP@users.noreply.github.com>
2024-10-06 23:03:19 +0530

    root correction

--- Components/Internships/CreateInternshipPost.jsx
+++ Components/Internships/CreateInternshipPost.jsx
@@ -89,7 +89,7 @@ const CreateInternshipPost = ({
   return (
     <div className="font-default text-dark-blue">
       <div className="flex justify-center mt-4">
-        <div className="!w-7/12 shadow p-6 bg-white rounded">
+        <Card className="!w-7/12 shadow">
           <Form
             form={form}
             layout="vertical"
@@ -264,7 +264,7 @@ const CreateInternshipPost = ({
               </Button>
             </div>
           </Form>
-        </div>
+        </Card>
       </div>
     </div>
   );
